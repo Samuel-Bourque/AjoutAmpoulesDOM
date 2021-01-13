ac5b8c06c2cc00e4590c43314a6123305e836ea5
Samuel Bourque <70170253+Samuel-Bourque@users.noreply.github.com>
2021-01-13 00:47:21 -0500

    Add files via upload

---
+++ labo5-3q5-Samuel-Bourque/Labo5_UI/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Labo5_UI
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+            public App()
+            {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        }
+
+    }
+}
---
+++ labo5-3q5-Samuel-Bourque/Labo5_UI/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace Labo5_UI
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AjouterUtilisation_Click(object sender, RoutedEventArgs e)
+        {
+            XmlDocument dom = new XmlDocument();
+            dom.Load("residence.xml");
+            var racine = dom.SelectSingleNode("/residence/utilisations");
+            XmlElement noeudUtilisation = dom.CreateElement("utilisation");
+            XmlElement noeudNbHeures = dom.CreateElement("nbHeures");
+            XmlElement noeudNom = dom.CreateElement("nom");
+            XmlElement noeudPuissanceWatts = dom.CreateElement("puissanceWatts");
+            noeudNbHeures.AppendChild(dom.CreateTextNode("1.5"));
+            noeudNom.AppendChild(dom.CreateTextNode("Ampoule"));
+            noeudPuissanceWatts.AppendChild(dom.CreateTextNode("60"));
+            noeudUtilisation.AppendChild(noeudNbHeures);
+            noeudUtilisation.AppendChild(noeudNom);
+            noeudUtilisation.AppendChild(noeudPuissanceWatts);
+            racine.AppendChild(noeudUtilisation);
+            dom.Save("residence.xml");
+        }
+
+        private void CalculerDureeAmpoules_Click(object sender, RoutedEventArgs e)
+        {
+            using (XmlReader reader = XmlReader.Create("residence.xml"))
+            {
+                string chaine = "";
+                string nom = "";
+                decimal nbHeures = 0.0m;
+                decimal total = 0.0m;
+
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            break;
+                        case XmlNodeType.Text:
+                            chaine = reader.Value.Trim();
+                            break;
+                        case XmlNodeType.EndElement:
+                            if (reader.Name.Equals("nom"))
+                                nom = chaine;
+                            if (reader.Name.Equals("nbHeures"))
+                                nbHeures = Decimal.Parse(chaine);
+                            if (reader.Name.Equals("utilisation"))
+                            {
+                                if (nom == "Ampoule")
+                                {
+                                    total += nbHeures;
+                                }
+                            }
+                            break;
+                    }
+                }
+                tbDureeTotale.Text = total.ToString();
+            }
+        }
+    }
+}
